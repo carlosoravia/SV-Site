00face233303cfba400f8916e1b0a317f5ae42f3
carlosoravia <carlo.soravia99@gmail.com>
2022-11-28 13:33:52 +0100

    hero in costruzione

--- resources/views/components/section-foto.blade.php
+++ resources/views/components/section-foto.blade.php
@@ -1,15 +1,12 @@
-<div class="container-fluid w-75 m-4 mx-auto shadow-lg">
-    <div class="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border shadow-lg bg-dark text-secondary">
-        <div class="col-lg-7 p-lg-5 pt-lg-3">
+<div class="container-fluid mx-auto shadow-lg">
+    <div class="row align-items-center rounded-3 border shadow-lg bg-dark text-secondary" id="borderNav">
+        <div class="col-12 text-center p-5">
             <h1 class="display-4 fw-bold lh-1 text-white">MEGA LOREM</h1>
             <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quisquam, consequuntur praesentium quod dolorum quasi quae libero quas sapiente deserunt impedit voluptatem vitae, qui facere ab consequatur nostrum similique quam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi totam ex consequatur nam saepe ipsa? Impedit est consectetur suscipit officiis quod, enim ullam, corrupti cum quos quam, aut repudiandae quis.</p>
-            <div class="d-grid gap-2 d-md-flex justify-content-md-start mb-4 mb-lg-3">
-                <button type="button" class="btn btn-outline-primary btn-lg px-4 me-md-2 fw-bold">Per i comuni</button>
-                <button type="button" class="btn btn-outline-warning btn-lg px-4">Per le aziende</button>
+            <div class="d-flex justify-content-center mt-4">
+                <button type="button" class="btn btn-outline-primary btn-lg px-4 mx-5 me-md-2 fw-bold">Per i comuni</button>
+                <button type="button" class="btn btn-outline-warning btn-lg px-4 mx-5">Per le aziende</button>
             </div>
-        </div>
-        <div class="col-lg-4 offset-lg-1 overflow-hidden">
-            <img class="rounded-lg-3 img-fluid" src="https://picsum.photos/400/300">
         </div>
     </div>
 </div>
